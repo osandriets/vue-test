193c0fda6133f4b702a73ef8772b45e4e4cd56b6
osadnriets@gmail.com <gjx2890-12345>
2018-04-03 15:59:12 +0300

    add style for app-ordering-right-panel

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="app-container yyy">
-    <h1>Оформление заказа</h1>
     <div class="ordering">
+      <h1 class="ordering__title">Оформление заказа</h1>
       <div class="ordering-left-panel">
           <button class="btn btn-primary"
                   :class="{active: selectedComponent == 'app-ordering-new-buyer'}"
@@ -19,7 +19,7 @@
             <component :is="selectedComponent"></component>
           </keep-alive>
       </div>
-      <app-ordering-right-panel/>
+      <app-ordering-right-panel class="ordering-right-panel"/>
     </div>
   </div>
 </template>
@@ -44,17 +44,40 @@ export default {
 </script>
 
 <style scoped lang="scss">
+  @import "../assets/scss/variables";
   .app-container{
-    width: 600px;
-    margin: 0 auto;
+    width: 100%;
+    min-width: $min-width;
+    max-width: $max-width;
+    padding-left: $grid-gutter-width/2;
+    padding-right: $grid-gutter-width/2;
+    margin-left: auto;
+    margin-right: auto;
   }
   .ordering{
     display: flex;
+    flex-wrap: wrap;
+  }
+  .ordering__title{
+    width: 100%;
+    padding-left: $grid-gutter-width/2;
+    padding-right: $grid-gutter-width/2;
+    font-size: 32px;
   }
   .ordering-left-panel{
-    width: 30%;
+    width: 100%;
+    padding-left: $grid-gutter-width/2;
+    padding-right: $grid-gutter-width/2;
+    @media screen and(min-width: map-get($breakpoints, m)) {
+      width: 100% * 2 / 3;
+    }
   }
-  .ordering-left-panel{
-    width: 30%;
+  .ordering-right-panel{
+    width: 100%;
+    padding-left: $grid-gutter-width/2;
+    padding-right: $grid-gutter-width/2;
+    @media screen and(min-width: map-get($breakpoints, m)) {
+      width: 100% * 1 / 3;
+    }
   }
 </style>
--- src/components/OrderingRightPanel.vue
+++ src/components/OrderingRightPanel.vue
@@ -1,13 +1,23 @@
 <template>
-    <div class="">
-        <div>
-            <div v-for="item in contacts" v-bind:key="item.id">
-                <a href="item.url">{{item.name}}</a>
-                <p>{{item.price}} грн. x {{item.quantity}} шт.</p>
-                <p>{{item.price * item.quantity}} грн.</p>
+    <div>
+        <div class="order-card">
+            <div class="order-card__header">Ваш заказ</div>
+            <div class="order-card__line" v-for="item in contacts" v-bind:key="item.id">
+                <div class="order-card__block_img">
+                    <img class="order-card__img" :src="item.url_img"/>
+                </div>
+                <div class="order-card__text">
+                    <p>
+                        <a href="item.url">{{item.name}}</a>
+                    </p>
+                    <div class="order-card__block_price">
+                        <div class="order-card__text_price">{{item.price}} грн. x {{item.quantity}} шт.</div>
+                        <div class="order-card__text_total">{{item.price * item.quantity}} грн.</div>
+                    </div>
+                </div>
             </div>
+            <div class="order-card__footer">{{ reversedMessage }} грн.</div>
         </div>
-        <div>{{ reversedMessage }}</div>
     </div>
 </template>
 <script>
@@ -16,14 +26,14 @@ export default {
     return {
       contacts: [
         {
-          url_img: false,
+          url_img: 'static/img1.png',
           name: 'Цифровий фотоапарат Nikon D5100 kit (18-55 VR)',
           url: '#',
           price: 6568,
           quantity: 1
         },
         {
-          url_img: false,
+          url_img: 'static/img2.png',
           name: 'MP3-плеер Apple iPod Touch 5Gen 32Gb blue',
           url: '#',
           price: 3689,
@@ -43,3 +53,55 @@ export default {
   }
 }
 </script>
+<style scoped lang="scss">
+    @import "../assets/scss/variables";
+    .order-card{
+        background: $inverse-color;
+        border: 1px solid $border-color;
+        border-radius: $border-radius-base;
+        font-size: 14px;
+    }
+    .order-card__header{
+        font-size: 18px;
+        font-weight: bold;
+        border-bottom: $border-width dotted $border-color;
+        padding: map-get($spacers, 3) map-get($spacers, 3);
+    }
+    .order-card__line{
+        padding: map-get($spacers, 3) map-get($spacers, 3);
+        border-bottom: $border-width dotted $border-color;
+        display: flex;
+    }
+    .order-card__block_img{
+        width: 68px;
+        height: 68px;
+        margin-right: map-get($spacers, 2);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    .order-card__img{
+        max-width: 100%;
+        max-height: 100%;
+    }
+    .order-card__text{
+        flex: 1;
+    }
+    .order-card__block_price{
+        display: flex;
+        justify-content: space-between;
+    }
+    .order-card__text_price{
+        color: $text-muted;
+    }
+    .order-card__text_total{
+        font-weight: bold;
+    }
+
+    .order-card__footer{
+        font-size: 18px;
+        font-weight: bold;
+        text-align: right;
+        padding: map-get($spacers, 3) map-get($spacers, 3);
+    }
+</style>
